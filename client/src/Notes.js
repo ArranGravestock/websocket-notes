e18266d19b99450238f9adcc9195402864647869
Arran <arran@gravestock.net>
2019-10-19 15:41:12 +0100

    fix scss importing

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './Notes.css'
 
 class Note extends Component {
     constructor(props) {
--- client/src/toolbar.js
+++ client/src/toolbar.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import './Toolbar.css'
-
 import Editor from './Editor'
 
 
